fec75d2fc912afb4bce2029310f1bd4cbcd7c566
Henrique_Nardy <hnardyluz@gmail.com>
2022-10-17 23:32:33 -0300

    atualização_da_documentação

--- linha.py
+++ linha.py
@@ -1,7 +1,6 @@
 #criação de uma linha simples com a biblioteca math plot lib
 
 import matplotlib.pyplot as plt
-
 x = [1 , 2 , 3] #valores de x e y respectivos há uma linha de 45 graus no plano cartesiano
 y = [1 , 2 , 3]
 plt.plot(x,y)
